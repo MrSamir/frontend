0d5328390ac984294142f2f889140db0799a0dc0
AWQAF\a.madany.c <AWQAF\a.madany.c>
2023-06-11 15:38:30 +0300

    ESD-35
      [Apply] apply bootstrap theme
      [Create] create  common style for all the applications inside the workspace
     @Integrate-PrimeNG-UI-Framework-employee Portal

--- workspace/projects/employee-portal/src/app/app.module.ts
+++ workspace/projects/employee-portal/src/app/app.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import {CoreModule} from "./core/core.module";
+import { ButtonModule } from 'primeng/button';
  
 @NgModule({
   declarations: [
@@ -12,8 +13,9 @@ import {CoreModule} from "./core/core.module";
   imports: [
     BrowserModule,
     AppRoutingModule,
-    CoreModule
-    
+    CoreModule,
+    BrowserAnimationsModule ,
+    ButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
