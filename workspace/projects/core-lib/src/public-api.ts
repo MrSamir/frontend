90807df9f71e6c19e1179cfd1f7d83735a3036f7
AWQAF\a.madany.c <AWQAF\a.madany.c>
2023-06-13 09:43:17 +0300

    ESD-32
    Develop generic service for loading global application setting from JSON file
    @Configure-Angular-portal-to-load-application-configuration

--- workspace/projects/core-lib/src/public-api.ts
+++ workspace/projects/core-lib/src/public-api.ts
@@ -5,3 +5,4 @@
 export * from './lib/core-lib.service';
 export * from './lib/core-lib.component';
 export * from './lib/core-lib.module';
+export * from './lib/application-configuration-loader/application-configuration-loader.module'
--- workspace/projects/employee-portal/src/app/app.module.ts
+++ workspace/projects/employee-portal/src/app/app.module.ts
@@ -5,7 +5,19 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import {CoreModule} from "./core/core.module";
 import { ButtonModule } from 'primeng/button';
- 
+import { /*...,*/ APP_INITIALIZER } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+import { ApplicationConfigurationLoaderModule } from 'projects/core-lib/src/public-api';
+import { AppconfigurationLoaderService } from 'projects/core-lib/src/lib/application-configuration-loader/appconfiguration-loader.service';
+import { AppConfig } from './app-config';
+
+export function setupAppConfigServiceFactory(
+  service:AppconfigurationLoaderService, 
+  appConfigSetting?: AppConfig
+): Function {
+  return () => service.load('app.config.json',appConfigSetting);
+}
+
 @NgModule({
   declarations: [
     AppComponent
@@ -15,9 +27,21 @@ import { ButtonModule } from 'primeng/button';
     AppRoutingModule,
     CoreModule,
     BrowserAnimationsModule ,
-    ButtonModule
+    ButtonModule,
+    HttpClientModule,
+    ApplicationConfigurationLoaderModule
   ],
-  providers: [],
+  providers: [
+    {
+      provide: APP_INITIALIZER,
+        useFactory: setupAppConfigServiceFactory,
+        deps: [
+            AppconfigurationLoaderService
+        ],
+        multi: true
+    }
+    
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
