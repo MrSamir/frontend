9a9724d8182ee8f1387d4b321b2b4427f219593e
Kareem Fahmy <k.fahmy.c@awqaf.gov.sa>
2023-06-15 09:01:51 +0300

    finish bread crumb

--- workspace/projects/shared-features-lib/src/lib/shared-component/shared-component-module/sharecomponent.module.ts
+++ workspace/projects/shared-features-lib/src/lib/shared-component/shared-component-module/sharecomponent.module.ts
@@ -1,19 +1,19 @@
-import { NgBootstrapHijriGregorianDatepickerModule } from './../../ng-bootstrap-hijri-gregorian-datepicker/ng-bootstrap-hijri-gregorian-datepicker.module';
+//import { NgBootstrapHijriGregorianDatepickerModule } from './../../ng-bootstrap-hijri-gregorian-datepicker/ng-bootstrap-hijri-gregorian-datepicker.module';
 import { NgModule } from '@angular/core';
 
 import { BreadcrumbComponent } from '../breadcrumb/breadcrumb.component';
 import { BreadcrumbModule } from 'primeng/breadcrumb';
-import { NgBootstrapHijriGregorianDatepickerComponent } from '../../ng-bootstrap-hijri-gregorian-datepicker/ng-bootstrap-hijri-gregorian-datepicker.component';
+//import { NgBootstrapHijriGregorianDatepickerComponent } from '../../ng-bootstrap-hijri-gregorian-datepicker/ng-bootstrap-hijri-gregorian-datepicker.component';
 
 
 
 @NgModule({
   declarations: [BreadcrumbComponent ],
   imports: [
-    BreadcrumbModule,NgBootstrapHijriGregorianDatepickerModule
+    BreadcrumbModule//,NgBootstrapHijriGregorianDatepickerModule
   ],
   exports:[
-    BreadcrumbComponent,NgBootstrapHijriGregorianDatepickerComponent
+    BreadcrumbComponent//,NgBootstrapHijriGregorianDatepickerComponent
   ]
 
 })
