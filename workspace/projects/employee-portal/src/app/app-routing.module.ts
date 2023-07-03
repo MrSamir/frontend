42bd80ca994ab209fc8d2b59c0ffb240b8877908
Mohammed Samir <m.eldesouky.c@awqaf.gov.sa>
2023-07-03 16:40:31 +0300

    Adding bread crumb and fonts

--- workspace/projects/employee-portal/src/app/app-routing.module.ts
+++ workspace/projects/employee-portal/src/app/app-routing.module.ts
@@ -7,15 +7,15 @@ import { PagetwoComponent } from './core/compoenents/pagetwo/pagetwo.component';
 import { LoginComponent } from './core/compoenents/login/login.component';
 
 const routes: Routes = [
-  { path: '', redirectTo: 'home', pathMatch: 'full' },
- 
+  // { path: '', redirectTo: 'home', pathMatch: 'full' },
+  
   { path: '', component: LoginComponent }, // Update with your default component
 
 
 
   {
     path: 'employee',
-    loadChildren: () => import('projects/employee-portal/src/app/modules/employee-portal.module').then(m => m.EmployeePortalModule)
+    loadChildren: () => import('projects/employee-portal/src/app/modules/employee-portal.module').then(m => m.EmployeePortalModule),data: { breadcrumb: 'منصة الاستعلامات' } 
    
   }
  
