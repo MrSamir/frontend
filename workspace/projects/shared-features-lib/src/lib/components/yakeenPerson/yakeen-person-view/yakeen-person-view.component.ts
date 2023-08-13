160772862c5625197333c12b432b9d53d468445b
Mohamed Shaaban <mohshaban@elm.sa>
2023-08-13 13:40:11 +0300

    finishing yqueen and endowmers task front end

--- workspace/projects/shared-features-lib/src/lib/components/yakeenPerson/yakeen-person-view/yakeen-person-view.component.ts
+++ workspace/projects/shared-features-lib/src/lib/components/yakeenPerson/yakeen-person-view/yakeen-person-view.component.ts
@@ -17,59 +17,24 @@ export class YakeenPersonViewComponent implements OnInit {
   constructor(public  lookupService: LookupApplicationServiceServiceProxy) {
   }
 
-  @Input() citizen: CitizenInfoResponse;
-  @Input() alien: AlienInfoResponse;
+  @Input() citizen: CitizenInfoResponse | undefined;
+  @Input() alien: AlienInfoResponse | undefined;
   @Input() person: OutputApplicationUserDto;
   @Input() isCitizen: boolean;
   @Input() isHafeza:boolean;
   lookupfliter:InputLookUpDto=new InputLookUpDto();
-  IdTypeLookup:any=[];
+  @Input() IdTypeLookup:any=[];
   _lookupExtraData:   LookupExtraData=new LookupExtraData();
 
-  // idTypeLables = [
-  //   'هوية وطنية',
-  //   'إقامة',
-  //   'حفيظة نفوس',
-  //   'اخرى'
-  // ];
-  //
   get selectedTypeName() {
-    this.LoadIdTypes(this.person.idTypeId || 0);
-    return this.IdTypeLookup[0].name;
-  }
-
-  // get dateLabel() {
-  //   if(this.isCitizen){
-  //     return translations.dateOfBirthHijri;
-  //   }else{
-  //     if(this.alien.idTypeId === IdType.IQAMA_ONLY){
-  //       return  translations.dateOfBirthGregorian;
-  //     }else {
-  //       return translations.dateOfBirthHijri;
-  //     }
-  //   }
-  // }
-
-  LoadIdTypes(Id: number)
-  {
-    this._lookupExtraData.dataName="Id"
-    this._lookupExtraData.dataValue=Id.toString();
-    this.lookupfliter.lookUpName="IdTypes";
-    this.lookupfliter.filters=[this._lookupExtraData];
-    this.lookupService.getAllLookups(this.lookupfliter).subscribe(
-      (data) => {
-        this.IdTypeLookup=data.dto.items;
-        console.log(data);
-      }
-    );
-
+    return this.IdTypeLookup[0]?.name;
   }
   get birthDate() {
     if(this.isCitizen){
-      return this.citizen.dateOfBirthH ;
+      return this.citizen?.dateOfBirthH ;
     }
     else{
-        return this.alien.dateOfBirthG;
+        return this.alien?.dateOfBirthG;
     }
   }
 
