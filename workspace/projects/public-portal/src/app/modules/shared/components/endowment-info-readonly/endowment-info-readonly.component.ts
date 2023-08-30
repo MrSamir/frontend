2008de8c7c0ab0b25a4cebe28240a2aab5c97bf2
Mohamed Shaban <m.shaban.c@awqaf.gov.sa>
2023-08-30 16:41:00 +0300

    fixing attachment in endowment info readonly

--- workspace/projects/public-portal/src/app/modules/shared/components/endowment-info-readonly/endowment-info-readonly.component.ts
+++ workspace/projects/public-portal/src/app/modules/shared/components/endowment-info-readonly/endowment-info-readonly.component.ts
@@ -39,7 +39,7 @@ import { ComponentBase } from 'projects/core-lib/src/lib/components/ComponentBas
   templateUrl: './endowment-info-readonly.component.html',
   styleUrls: ['./endowment-info-readonly.component.css']
 })
-export class EndowmentInfoReadonlyComponent  extends ComponentBase implements OnInit {
+export class EndowmentInfoReadonlyComponent extends ComponentBase implements OnInit {
   @Input() public IsCreate = true;
   @Input() InputEndowmentDto: InputEndowmentDto = new InputEndowmentDto();
   @Input() waqfId: string;
@@ -320,7 +320,11 @@ export class EndowmentInfoReadonlyComponent  extends ComponentBase implements On
       .subscribe(
         (result: ApiResponse) => {
           if (result.isSuccess) {
+            debugger;
             this.InputEndowmentDto = result.dto;
+            if (this.InputEndowmentDto.endowmentDeedAttachmentId == '00000000-0000-0000-0000-000000000000') {
+              this.InputEndowmentDto.endowmentDeedAttachmentId = undefined;
+            }
             if (this.InputEndowmentDto.endowmentDeedRegionId) {
               this.LoadCitiesByRegion(this.InputEndowmentDto.endowmentDeedRegionId);
             }
